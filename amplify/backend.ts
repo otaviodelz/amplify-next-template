24261121c15853c9e9fe1b86327a69145da16a62
Otavio Zirpoli <otavio.zirpoli@compasso.com.br>
2025-02-20 13:57:26 -0300

    add custom storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,15 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource.js";
+import { data } from "./data/resource.js";
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: "us-east-2",
+    bucket_name: "lz-storage-browser",
+  },
 });

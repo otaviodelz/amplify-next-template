9b3d1f95db7409714e409f0629f31dc5aa7918fb
Otavio Zirpoli <otavio.zirpoli@compasso.com.br>
2025-02-20 15:24:08 -0300

    add auth and protected route

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,6 +3,10 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: "amplifyStorageTest",
   access: (allow) => ({
-    "public/*": [allow.guest.to(["read"])],
+    "public/*": [
+      allow.guest.to(["read"]),
+      allow.authenticated.to(["read", "write", "delete"]),
+    ],
+    "protected/*": [allow.authenticated.to(["read", "write", "delete"])],
   }),
 });
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,21 @@
 import "./../app/app.css";
 import "@aws-amplify/ui-react/styles.css";
 import { StorageBrowser } from "./components/storage-browser";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 export default function App() {
   return (
     <main>
-      <h1>My Storage Browser</h1>
-      <StorageBrowser />
+      <h1>Storage Browser</h1>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div>
+            <h1>user: {user?.username}</h1>
+            <button onClick={signOut}>Logout</button>
+            <StorageBrowser />
+          </div>
+        )}
+      </Authenticator>
     </main>
   );
 }

8c6dcd53e82e3fe852b181f4e6d874bd99f23646
Otavio Zirpoli <otavio.zirpoli@compasso.com.br>
2025-02-20 14:47:36 -0300

    add storage access

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,7 @@ import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
   name: "amplifyStorageTest",
+  access: (allow) => ({
+    "/*": [allow.guest.to(["read"])],
+  }),
 });
